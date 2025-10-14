a13edbb84736dfe32e61a5f110f380972790965e
nizarnifo <nizarnifo@gmail.com>
2025-10-15 01:00:22 +0200

    Merge remote-tracking branch 'origin/main'

--- back-end/src/functions/crypto.js
+++ back-end/src/functions/crypto.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 exports.randomToken = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
 exports.sha256 = (str) => crypto.createHash('sha256').update(str).digest('hex');
-exports.b64url = (obj) => Buffer.from(JSON.stringify(obj)).toString('base64url');
+exports.b64url =  (obj) => Buffer.from(JSON.stringify(obj)).toString('base64url');
 
 
 

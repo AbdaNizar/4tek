71ba2fe13f95f844ddba557a0091a6c0e948daec
nizarnifo <nizarnifo@gmail.com>
2025-10-15 11:53:51 +0200

    new dist

--- front-end/dist/4tek/browser/assets/env.js
+++
@@ -1,1 +0,0 @@
-(function(w){ w.__env = { API_BASE_URL: "http://localhost:3000/v1" }; })(window);
--- front-end/dist/4tek/browser/assets/env.template.js
+++
@@ -1,5 +0,0 @@
-(function (w) {
-  w.__env = {
-    API_BASE_URL: "${API_BASE_URL}" // injectée depuis le .env backend au déploiement
-  };
-})(window);
